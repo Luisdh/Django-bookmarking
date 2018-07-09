3d48a8dbd115500fa22e2ceda00eee6a162b9cc6
unknown <luisd824@gmail.com>
2018-07-09 18:20:41 -0400

    ADD BOOKMARKS
    - create boomark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class BookmarkBase(models.Model):
+  title = models.CharField(max_length=100, blank=True)
+  created_on = models.DateTimeField(auto_now_add=True)
+  url = models.URLField(blank=True)
+  description = models.TextField(blank=True)
+
+class Bookmark(BookmarkBase):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- flags/settings.py
+++ flags/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bm&-qg=#2msj_4cch@nn5hs1iyu*=ee6pr-#)zg&_4$d5eq$%('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bookmarks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
